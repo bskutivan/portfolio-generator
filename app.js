b5ec40fd9aff80b9185ec2a339b2825ba8f60a11
BrandonKutivan <bskutivan@gmail.com>
2020-05-09 13:47:02 -0700

    beefed up html generation to take more data using filter and map methods as well as array destructuring.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const inquirer = require('inquirer');
 
+const fs = require('fs');
+
+const generatePage = require('./src/page-template');
+
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -125,12 +129,16 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-  console.log(portfolioData);
+  const pageHTML = generatePage(portfolioData);
+
+  fs.writeFile('index.html', pageHTML, err => {
+      if (err) throw err;
+
+      console.log('Portfolio complete! Check out index.html to see the output!')
+  });
 });
 
-// const fs = require('fs');
 
-// const generatePage = require('./src/page-template.js');
 
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 
